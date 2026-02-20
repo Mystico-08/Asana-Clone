b19aed75686809155c84c29b96d64845ec11eca2
Mystico-08 <gantavyamishra@gmail.com>
2026-02-20 23:16:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleMenu(id) {
+
+  const menus = document.querySelectorAll(".mega-menu");
+
+  menus.forEach(menu => {
+    if (menu.id === id) {
+      menu.classList.toggle("active");
+    } else {
+      menu.classList.remove("active");
+    }
+  });
+
+}
+
+function openMobile() {
+  document.getElementById("mobileMenu").classList.add("active");
+}
+
+function closeMobile() {
+  document.getElementById("mobileMenu").classList.remove("active");
+  document.getElementById("mobileContent").innerHTML = "";
+}
+
+function toggleMobileMega(id) {
+  const content = document.getElementById("mobileContent");
+  const menu = document.getElementById(id);
+  content.innerHTML = menu.innerHTML;
+}
